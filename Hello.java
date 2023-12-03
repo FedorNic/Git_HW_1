133ad201193ad9fbad5c19d4d6f531889bb79461
FedorNic <dmitrievfedor@yandex.ru>
2023-12-03 14:55:37 +0300

    hello_again

--- Hello.java
+++ Hello.java
@@ -6,5 +6,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
         System.out.println("Yes");
+        System.out.println("Hello, again!");
     }
 }
